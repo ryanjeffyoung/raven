1211ed07c81a121e71dcd948b1a2b0382ac2368d
ryanjeffyoung <ryan.jeffery.young@gmail.com>
2020-07-06 18:14:28 -0700

    pt 3 start

---
+++ src/components/App/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from 'C:/Users/daily/Code/ReactJS/Codecademy-Raven/raven/src/logo.svg';
+import './App.css';
+import '../BusinessList/BusinessList';
+import '../SearchBar/SearchBar';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses}/>
+      </div>
+    )
+  }
+}
+
+export default App;
